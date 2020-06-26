eacde7dd681ad24aa25089ac7fc323a9cf47d096
ubuntu <peter.todorov@live.com>
2020-06-26 11:15:51 +0300

    Clean EarlyStopper callback

--- callbacks.py
+++ callbacks.py
@@ -5,17 +5,21 @@ import pandas as pd
 
 
 class EarlyStopping:
-    """Early stops the training if validation loss doesn't improve after a given patience."""
+    """
+    Early stops the training if validation loss doesn't improve after a 
+    given patience.
+    """
     def __init__(
             self, save_model_directory, patience=10, verbose=False, delta=0):
         """
         Args:
-            patience (int): How long to wait after last time validation loss improved.
-                            Default: 7
-            verbose (bool): If True, prints a message for each validation loss improvement. 
-                            Default: False
-            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
-                            Default: 0
+            patience : int
+                How long to wait after last time validation loss improved.
+            verbose : bool
+                If True, prints a message for each validation loss improvement. 
+            delta : float
+                Minimum change in the monitored quantity to qualify as an
+                improvement.
         """
         self.patience = patience
         self.verbose = verbose
@@ -39,7 +43,7 @@ class EarlyStopping:
             self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
-            print(f'EarlyStopping counter: {self.counter}/{self.patience}')
+            print(f"EarlyStopping counter: {self.counter}/{self.patience}")
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
@@ -48,11 +52,11 @@ class EarlyStopping:
             self.counter = 0
 
     def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
+        """Saves model when validation loss decrease."""
         if self.verbose:
-            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), "{}/model.pt".format(
-            self.save_directory))
+            print("Validation loss decreased. Model saved")
+        torch.save(
+            model.state_dict(), os.path.join(self.save_directory, "model.pt"))
         self.val_loss_min = val_loss
 
     def isEarlyStop(self):
