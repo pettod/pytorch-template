21d574d7fc6a1309c2c1a2036ae9be961339a878
Peter Todorov <peter.todorov@live.com>
2021-09-20 13:24:38 +0300

    Add 'piq' metrics for PSNR and SSIM

--- src/metrics.py
+++ src/metrics.py
@@ -1,8 +1,10 @@
-import torch
+from piq import psnr as psnr_
+from piq import ssim as ssim_
 
 
-def psnr(y_true, y_pred, max_pixel_value=2.0):
-    mse = torch.mean(torch.square(y_true - y_pred))
-    if mse == 0:
-        return 100
-    return 10 * torch.log10(max_pixel_value**2 / mse)
+def psnr(y_true, y_pred):
+    return psnr_(y_true, y_pred)
+
+
+def ssim(y_true, y_pred):
+    return ssim_(y_true, y_pred)
