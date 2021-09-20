d49dd8dab483bfeda234951007c47452ad2c69a9
Peter Todorov <peter.todorov@live.com>
2021-09-20 13:30:33 +0300

    Fix transforms to work with Pytorch >=1.6.0 and make dataset work with testing
    Return input and ground truth is ground truth path provided, otherwise return input

--- src/dataset.py
+++ src/dataset.py
@@ -10,22 +10,36 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 
 
+def readImage(image_path):
+    return Image.open(image_path).convert("RGB")
+
+
 class ImageDataset(Dataset):
-    def __init__(self, input_path, target_path, transform=None):
+    def __init__(self, input_path, target_path=None, transform=None):
         self.input_image_paths = np.array(sorted(glob(f"{input_path}/*/*.png")))
-        self.target_image_paths = np.array(sorted(glob(f"{target_path}/*/*.png")))
+        self.target_image_paths = target_path
+        if target_path:
+            self.target_image_paths = np.array(sorted(glob(f"{target_path}/*/*.png")))
         self.transform = transform
+        self.input_normalize = transforms.Normalize(
+            mean=[0.5, 0.5, 0.5],
+            std=[0.5, 0.5, 0.5])
 
     def __len__(self):
-        return len(self.target_image_paths)
+        return len(self.input_image_paths)
 
     def __getitem__(self, sample_index):
-        input_image = Image.open(self.input_image_paths[sample_index])
-        target_image = Image.open(self.target_image_paths[sample_index])
+        seed = random.randint(0, 2**32)
+        input_image = readImage(self.input_image_paths[sample_index])
         if self.transform:
-            seed = random.randint(0, 2**32)
-            random.seed(seed)
-            input_image = self.transform(input_image)
-            random.seed(seed)
-            target_image = self.transform(target_image)
-        return np.array(input_image), np.array(target_image)
+            random.seed(seed)  # Pytorch <= 1.5.1
+            torch.manual_seed(seed)  # Pytorch 1.6.0
+            input_image = self.input_normalize(self.transform(input_image))
+        if self.target_image_paths is not None:
+            target_image = readImage(self.target_image_paths[sample_index])
+            if self.transform:
+                random.seed(seed)
+                torch.manual_seed(seed)
+                target_image = self.transform(target_image)
+            return input_image, target_image
+        return input_image
--- train.py
+++ train.py
@@ -11,16 +11,10 @@ def main():
         transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.5, 0.5, 0.5],
-            std=[0.5, 0.5, 0.5]),
     ])
     valid_transforms = transforms.Compose([
         transforms.CenterCrop(CONFIG.PATCH_SIZE),
         transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.5, 0.5, 0.5],
-            std=[0.5, 0.5, 0.5]),
     ])
     train_dataset = Dataset(
         CONFIG.TRAIN_X_DIR, CONFIG.TRAIN_Y_DIR, train_transforms)
