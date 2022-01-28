39e39e49afb68d92fd91e93236e16cc9d11b81d7
Peter Todorov <peter.todorov@live.com>
2022-01-28 18:46:01 +0100

    Update 'test.py' for the new changes in the code architecture

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 from imco import compareImages
 from importlib import import_module
-from torchvision import transforms
+from torchvision.transforms import Compose, CenterCrop, Normalize, ToTensor
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
@@ -16,13 +16,13 @@ from src.utils.utils import loadModel
 
 # Data paths
 DATA_ROOT = os.path.realpath("../input/REDS")
-VALID_X_DIR = os.path.join(DATA_ROOT, "val_blur/")
-VALID_Y_DIR = os.path.join(DATA_ROOT, "val_sharp/")
+X_DIR = os.path.join(DATA_ROOT, "valid_blur")
+Y_DIR = os.path.join(DATA_ROOT, "valid_sharp")
 
 # Model parameters
 MODEL_PATHS = [
-    "saved_models/2021-10-10_213030",
-    "saved_models/2021-10-10_213030",
+    "saved_models/2022-01-28_180347",
+    "saved_models/2022-01-28_180347",
 ]
 NAMES = [
     "Input",
@@ -34,32 +34,41 @@ PATCH_SIZE = 256
 DEVICE = torch.device("cuda")
 
 
+def loadModels():
+    models = []
+    for model_path in MODEL_PATHS:
+        config = import_module(os.path.join(
+            model_path, "codes.config").replace('/', '.')).CONFIG
+        model = nn.DataParallel(config.MODELS[0]).to(DEVICE)
+        loadModel(model, model_path=model_path)
+        models.append(model)
+    return models
+
+
+def saveImage(save_directory, image, image_name):
+    os.makedirs(save_directory, exist_ok=True)
+    cv2.imwrite(os.path.join(
+        save_directory, image_name), cv2.cvtColor(
+            image, cv2.COLOR_RGB2BGR))
+
+
 def main():
     # Dataset
-    valid_transforms = transforms.Compose([
-        transforms.CenterCrop(PATCH_SIZE),
-        transforms.ToTensor(),
+    transforms = Compose([
+        CenterCrop(PATCH_SIZE),
+        ToTensor(),
     ])
-    valid_dataset = ImageDataset(VALID_X_DIR, VALID_Y_DIR, valid_transforms)
-    valid_dataloader = DataLoader(
-        valid_dataset, batch_size=4, shuffle=False, num_workers=8)
+    input_normalize = Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    dataset = ImageDataset(X_DIR, Y_DIR, transforms, input_normalize)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=False, num_workers=8)
 
     # Save directory
-    save_directory = os.path.join(
-        "predictions", time.strftime("%Y-%m-%d_%H%M%S"))
+    save_directory = os.path.join("predictions", time.strftime("%Y-%m-%d_%H%M%S"))
     with torch.no_grad():
-
-        # Load models
-        models = []
-        for model_path in MODEL_PATHS:
-            config = import_module(os.path.join(
-                model_path, "config").replace('/', '.')).CONFIG
-            model = nn.DataParallel(config.MODELS[0]).to(DEVICE)
-            loadModel(model, model_path=model_path)
-            models.append(model)
+        models = loadModels()
 
         # Predict and save
-        for i, (x, y) in enumerate(tqdm(valid_dataloader)):
+        for i, (x, y) in enumerate(tqdm(dataloader)):
             x, y = x.to(DEVICE), y.numpy()
             predictions = [m(x).cpu().numpy() for m in models]
             x += 1
@@ -69,11 +78,7 @@ def main():
                 images = [x[j]] + [p[j] for p in predictions] + [y[j]]
                 images = [(255 * np.moveaxis(img, 0, -1)).astype(np.uint8) for img in images]
                 concat_image = compareImages(images, NAMES, True).astype(np.uint8)
-                if not os.path.isdir(save_directory):
-                    os.makedirs(save_directory)
-                cv2.imwrite(os.path.join(
-                    save_directory, f"{i}_{j}.png"), cv2.cvtColor(
-                        concat_image, cv2.COLOR_RGB2BGR))
+                saveImage(save_directory, concat_image, f"{i}_{j}.png")
 
 
 main()
