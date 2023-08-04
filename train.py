992c330385124b8395d8c6b1124536fafff8a0c9
Peter Todorov <33998401+pettod@users.noreply.github.com>
2023-08-04 12:44:54 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,23 @@
+import optuna
+
 from config import CONFIG
+from src.optunaobjective import Objective
 from src.trainer import Trainer
 
 
+TRAINING_PARAMETERS = [
+    {"accuracy": 0.99, "flops": 100000, "loss": 2.0, "seed": 24132423, "lr": 0.04, "epochs": 100, "features": 4, "conv_layers": 4},
+]
+USED_PARAMETERS = None #TRAINING_PARAMETERS[-1]
+
+
 def main():
-    trainer = Trainer(CONFIG.TRAIN_DATASET, CONFIG.VALID_DATASET)
-    trainer.train()
+    if USED_PARAMETERS:
+        obj = Objective()
+        obj(optuna.trial.FixedTrial(USED_PARAMETERS), save_model=True)
+    else:
+        trainer = Trainer(CONFIG)
+        trainer.train()
 
 
 main()
